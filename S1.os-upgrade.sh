d29d44caaa9efc7f43e1e348776cb814e9b530b9
vBOSS.Tech <vbosstech@gmail.com>
2019-05-05 09:49:16 +0700

    no message

--- S1.os-upgrade.sh
+++ S1.os-upgrade.sh
@@ -182,7 +182,7 @@ if [ "`which docker`" = "" ]; then
   sudo curl -fsSL get.docker.com -o get-docker.sh
   sh get-docker.sh
   sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod u+x /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
   sudo groupadd docker
   sudo usermod -aG docker $USER
   sudo service docker restart
